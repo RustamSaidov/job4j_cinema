fef15fa753a2ff091aeb92b7c520fad280f4b8d9
rusta <rustam_ussr@mail.ru>
2023-03-07 09:39:57 +0300

    2. Сервис - Кинотеатр [#504869]

--- src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
+++ src/main/java/ru/job4j/cinema/repository/Sql2oUserRepository.java
@@ -1,8 +1,8 @@
 package ru.job4j.cinema.repository;
 
 import net.jcip.annotations.ThreadSafe;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.sql2o.Sql2o;
 import ru.job4j.cinema.model.User;
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class Sql2oUserRepository implements UserRepository {
 
     private final Sql2o sql2o;
-    private static final Logger LOG = LogManager.getLogger(Sql2oUserRepository.class.getName());
+    static final Logger LOG = LoggerFactory.getLogger(Sql2oUserRepository.class);
 
     public Sql2oUserRepository(Sql2o sql2o) {
         this.sql2o = sql2o;
